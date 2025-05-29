d50b629e0c973be77e4e1381a6b21c59a39ec076
Toastyice <paul.schella@hotmail.de>
2025-05-29 12:06:40 +0200

    Add KeycloakInstanceConfig type

---
+++ api/v1alpha1/keycloakinstanceconfig_types.go
@@ -0,0 +1,93 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:validation:XValidation:rule=`!(has(self.clientSecret) && (has(self.username) || has(self.password)))`,message="clientSecret cannot be used together with username or password"
+// +kubebuilder:validation:XValidation:rule=`!(has(self.username) && !has(self.password)) && !(has(self.password) && !has(self.username))`,message="username and password must be provided together"
+type KeycloakInstanceConfigSpec struct {
+	// The URL of the Keycloak instance, before /auth/admin
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:XValidation:rule=`self.matches('^https?://([a-zA-Z0-9-]+\\.)*[a-zA-Z0-9-]+(:[0-9]{1,5})?(/.*)?$')`,message="url must be a valid HTTP or HTTPS URL"
+	Url string `json:"url"`
+
+	// The base path used for accessing the Keycloak REST API. Defaults to an empty string. Note that users of the legacy distribution of Keycloak will need to set this attribute to /auth
+	// +optional
+	// +kubebuilder:validation:XValidation:rule=`size(self) == 0 || self.startsWith('/')`,message="basePath must start with /"
+	// +kubebuilder:validation:XValidation:rule=`size(self) == 0 || self == '/' || !self.endsWith('/')`,message="basePath must not end with / (except for root path)"
+	BasePath string `json:"basePath"`
+
+	// The realm used by the provider for authentication. Defaults to master.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=255
+	// +kubebuilder:default=master
+	Realm string `json:"realm"`
+
+	// The client_id for the client that was created in the "Keycloak Setup" section. Use the admin-cli client if you are using the password grant.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:default=admin-cli
+	ClientId string `json:"clientId"`
+
+	// The secret for the client used by the provider for authentication via the client credentials grant. This can be found or changed using the "Credentials" tab in the client settings. This attribute is required when using the client credentials grant, and cannot be set when using the password grant.
+	// +optional
+	ClientSecret string `json:"clientSecret,omitempty"`
+
+	// The username of the user used by the provider for authentication via the password grant. Required when using the password grant, and cannot be set when using the client credentials grant.
+	// +optional
+	Username string `json:"username,omitempty"`
+
+	// The password of the user used by the provider for authentication via the password grant. Required when using the password grant, and cannot be set when using the client credentials grant.
+	// +optional
+	Password string `json:"password,omitempty"`
+
+	// Sets the timeout of the client when addressing Keycloak, in seconds. Defaults to 15.
+	// +optional
+	// +kubebuilder:default=15
+	Timeout int `json:"timeout"`
+
+	// Allows ignoring insecure certificates when set to true. Defaults to false. Disabling this security check is dangerous and should only be done in local or test environments!
+	// +optional
+	// +kubebuilder:default=false
+	TlsInsecureSkipVerify bool `json:"tlsInsecureSkipVerify"`
+
+	// Allows x509 calls using a CA certificate
+	// +optional
+	CaCert string `json:"caCert,omitempty"`
+
+	// A map of custom HTTP headers to add to each request to the Keycloak API.
+	// +optional
+	AdditionalHeaders map[string]string `json:"additionalHeaders,omitempty"`
+}
+
+// KeycloakInstanceConfigStatus defines the observed state of KeycloakInstanceConfig
+type KeycloakInstanceConfigStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// KeycloakInstanceConfig is the Schema for the keycloakinstanceconfigs API
+type KeycloakInstanceConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KeycloakInstanceConfigSpec   `json:"spec,omitempty"`
+	Status KeycloakInstanceConfigStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// KeycloakInstanceConfigList contains a list of KeycloakInstanceConfig
+type KeycloakInstanceConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []KeycloakInstanceConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&KeycloakInstanceConfig{}, &KeycloakInstanceConfigList{})
+}
--- cmd/main.go
+++ cmd/main.go
@@ -196,6 +196,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Group")
 		os.Exit(1)
 	}
+	if err = (&controller.KeycloakInstanceConfigReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "KeycloakInstanceConfig")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
